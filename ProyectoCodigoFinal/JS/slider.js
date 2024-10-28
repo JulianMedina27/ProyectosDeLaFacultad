a5cdbe8f57aab6f8e8a6e5b1060518f29af0ae4e
AgustinFava <afavarolo@alumno.unlam.edu.ar>
2024-10-28 19:16:41 -0300

    Slider

--- ProyectoCodigoFinal/JS/slider.js
+++ ProyectoCodigoFinal/JS/slider.js
@@ -1,3 +1,31 @@
+let sliderInner = document.querySelector(".slider-interior");
 
-let currentSlide = 0;
-const slides = document.querySelector('.slider img');
+let images = sliderInner.querySelectorAll("img");
+
+let index = 0;
+
+// Configura el carrusel automático
+function nextSlide() {
+    index = (index + 1) % images.length; // Cicla a través de las imágenes
+    updateSlide();
+}
+
+// Función para actualizar la diapositiva y el botón activo
+function updateSlide() {
+    let percentage = index * -100;
+    sliderInner.style.transform = `translateX(${percentage}%)`;
+    
+    // Actualiza el botón activo
+    document.querySelectorAll(".slider-nav button").forEach((btn, i) => {
+        btn.classList.toggle("active", i === index);
+    });
+}
+
+// Cambia a una diapositiva específica al hacer clic en el botón de navegación
+function selectSlide(slideIndex) {
+    index = slideIndex;
+    updateSlide();
+}
+
+// Configura el intervalo de auto-desplazamiento
+let interval = setInterval(nextSlide, 3000);
