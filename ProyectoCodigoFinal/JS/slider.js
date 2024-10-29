a99ca0cfb95faae1890b8baf9d81f83c3b75a334
AgustinFava <afavarolo@alumno.unlam.edu.ar>
2024-10-29 17:47:14 -0300

    Slider terminado

--- ProyectoCodigoFinal/JS/slider.js
+++ ProyectoCodigoFinal/JS/slider.js
@@ -1,56 +1,66 @@
-
+// Se crea un array con las imagenes y los links 
 let imagenes = [
     {
         "url": "img/java.jpg",
-
+        "link": "Paginas/CursoJava.html"
     },
     {
         "url": "img/curso-python-imagen.png",
-
+        "link": "Paginas/CursoPhyton.html"
     },
     {
         "url": "img/cursohtmlycss.jpg",
-        
+        "link": "Paginas/CursoHTMLyCSS.html"
     },
-]
+];
 
+//Selecciona a las flechas de atras y adelante, los puntos y al contenedor de imagenes
 let atras = document.getElementById('atras');
 let adelante = document.getElementById('adelante');
-let imagen = document.getElementById('img');
+let contenedorImagenes = document.getElementById('imagenes');
 let puntos = document.getElementById('puntos');
-let actual = 0
-posicionCarrusel()
+let actual = 0;         // Variable para almacenar la imagen actual mostrada
 
+// Mostrar la primera imagen al cargar la pagina
+actualizarImagen();
+posicionCarrusel();
+
+// Cambia la imagen por la anterior
 atras.addEventListener('click', function(){
-    actual -=1
-
-    if (actual == -1){
-        actual = imagenes.length - 1
+    actual -= 1;
+    if (actual == -1) {
+        actual = imagenes.length - 1;
     }
+    actualizarImagen();
+    posicionCarrusel();
+});
 
-    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
-    posicionCarrusel()
-})  
+// Cambia la imagen por la siguiente
 adelante.addEventListener('click', function(){
-    actual +=1
-
-    if (actual == imagenes.length){
-        actual = 0
+    actual += 1;
+    if (actual == imagenes.length) {
+        actual = 0;
     }
+    actualizarImagen();
+    posicionCarrusel();
+});
 
-    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
-    
-    posicionCarrusel()
-})  
+// Muestra la imagen actual
+function actualizarImagen() {
+    contenedorImagenes.innerHTML = `
+        <a href="${imagenes[actual].link}" target="_blank">
+            <img class="img" src="${imagenes[actual].url}" alt="imagen ${actual + 1}" loading="lazy">
+        </a>`;
+}
 
+// Marca al usuario en que posicion se encuentra haciendo que uno de los puntos pase a tener estilo bold
 function posicionCarrusel() {
-    puntos.innerHTML = ""
-    for (var i = 0; i <imagenes.length; i++){
-        if(i == actual){
-            puntos.innerHTML += '<p class="bold">.<p>'
+    puntos.innerHTML = "";
+    for (let i = 0; i < imagenes.length; i++) {
+        if (i === actual) {
+            puntos.innerHTML += '<p class="bold">.</p>';
+        } else {
+            puntos.innerHTML += '<p>.</p>';
         }
-        else{
-            puntos.innerHTML += '<p>.<p>'
-        }
-    } 
+    }
 }
