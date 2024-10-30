3b6b303b8ce40080ed14d56578f115e63347f172
FedericoCurutchet <DarkTheFkde@hotmail.com>
2024-10-29 21:24:13 -0300

    Detalle cursos
    Hay un contador para los cursos y cuando se da a comprar sale un modal, se guarda la cantidad de cursos comprados, se elimino de todas las paginas el menu de detalle cursos  ;c

---
+++ ProyectoCodigoFinal/JS/Curso_HTMLYCSS.js
@@ -0,0 +1,42 @@
+document.getElementById("menu-hamburguesa").addEventListener("click", function() {
+    var menu = document.getElementById("menu-horizontal");
+    menu.classList.toggle("menu-abierto");
+});
+
+const boton_inscribirse = document.getElementById('boton');
+const contenedor_contador = document.getElementById('contador');
+
+let contador_inscripcion = localStorage.getItem("contenedor_HYC");
+
+function setContador()
+{
+    contenedor_contador.textContent = `Cursos en Carrito: ${contador_inscripcion}`;
+}
+
+setContador();
+function agregarCurso()
+{
+    contador_inscripcion ++;
+    localStorage.setItem("contenedor_HYC",contador_inscripcion);
+    contenedor_contador.textContent = `Cursos en Carrito: ${contador_inscripcion}`;
+}
+
+boton_inscribirse.addEventListener('click', agregarCurso);
+
+// Selecciona el elementos del popup
+let titulo_texto = document.getElementById('titulo-popup');
+const modal_container = document.getElementById('modal-container');
+const close = document.getElementById('close');
+const vinculo = document.getElementById('vinculo');
+
+
+// A todos los botones se les agrega el evento click
+
+    boton_inscribirse.addEventListener('click', () => {
+        modal_container.classList.add('show');
+    });
+
+// Cierra el modal cuando se hace clic en el botón de cerrar
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
--- ProyectoCodigoFinal/JS/Cursos.js
+++
@@ -1,8 +0,0 @@
-const boton_inscribirse = document.getElementById('boton');
-
-boton_inscribirse.addEventListener('click', () => {
-if(){
-    
-}
-    
-});
